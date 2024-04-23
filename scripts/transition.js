1908d88407e123890ddbc1677ed90b61091822fe
Polga <henrique.polga@redes.ufsm.br>
2024-04-23 00:04:54 -0300

    mudancas230224

---
+++ scripts/transition.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const homeLink = document.getElementById('home');
+    homeLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        fadeOutEffect('home.html');
+    });
+
+    const mapaLink = document.getElementById('mapa');
+    mapaLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        fadeOutEffect('index.html');
+    });
+
+    const infoLink = document.getElementById('info');
+    infoLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        fadeOutEffect('info.html');
+    });
+
+    function fadeOutEffect(destination) {
+        const body = document.body;
+        body.style.filter = 'blur(3px)'; // Aplica o efeito de desfoque
+        setTimeout(function() {
+            setTimeout(function() {
+                window.location.href = destination;
+            }, 500); // Tempo (em milissegundos) para aguardar antes de redirecionar
+        }, 100); // Pequeno atraso para garantir que o desfoque seja aplicado antes da transição de opacidade
+    }
+});
+ 
